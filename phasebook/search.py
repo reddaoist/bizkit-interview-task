403680b931f8336be825769544a3552ba8dd961e
rhuver <rpedere96@gmail.com>
2022-10-19 21:20:17 +0800

    implement search algorithm plus bonus challenge

--- phasebook/search.py
+++ phasebook/search.py
@@ -20,11 +20,42 @@ def search_users(args):
             name: string
             age: string
             occupation: string
-
+  
     Returns:
         a list of users that match the search parameters
     """
 
     # Implement search here!
 
-    return USERS
+    result = []
+   
+    id_matches = []
+    name_matches = []
+    age_matches = []
+    occupation_matches = []
+
+
+    for user in USERS:
+        
+        if args.get('id') == user.get('id'):
+            id_matches.append(user)
+            continue
+        
+        if str(args.get('name')).casefold() in str(user.get('name')).casefold():
+            name_matches.append(user)
+            continue
+        
+        if int(user.get('age') or 0) in range(int(args.get('age') or 0) -1, int(args.get('age') or 0) +2):
+            age_matches.append(user)
+            continue
+        
+        if str(args.get('occupation')).casefold() in str(user.get('occupation')).casefold():
+            occupation_matches.append(user)
+            continue
+
+    result = id_matches + name_matches + age_matches + occupation_matches
+    return result
+
+
+    
+    
