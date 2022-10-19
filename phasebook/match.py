ac65e1ad98bee967c8ddfa9d643c29a1ea44bafa
rhuver <rpedere96@gmail.com>
2022-10-19 21:19:22 +0800

    modified matching algorithm

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,5 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    return set(fave_numbers_2).issubset(set(fave_numbers_1))
 
-    return True
