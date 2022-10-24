34e83199b0dbc66516b92676d769196778cc2b2b
rhuver <rpedere96@gmail.com>
2022-10-24 12:30:59 +0800

    add search implementation specs as comment

--- phasebook/v1/search.py
+++ phasebook/v1/search.py
@@ -27,6 +27,29 @@ def search_users(args):
 
     # Implement search here!
 
+    # Search Specifications
+
+    # All of the search parameters are optional. That means a user can 
+    #   pass no search parameter and the function should return all users.
+    #   The user can also pass just the id as a parameter and it should
+    #   just return the user with that id. The user can also pass multiple 
+    #   parameters and the function should return all the users that match 
+    #   ANY of the parameters provided.
+    # The id is unique. If the id is included in the search parameters,
+    #   then immediately include the user with the passed id.
+    # The name can be partially matched and is case insensitive. 
+    #   That means that if we have users with names John Doe, Jane Doe,
+    #   and Joe Doe, passing the word "doe" to the name parameter should 
+    #   include all of them in the results.
+    # The age parameter should include users that are in the range of 
+    #   age - 1 to age + 1. This means that if we pass 26 to the age parameter,
+    #   we should include users with ages 25 to 27.
+    # The occupation parameter can be partially matched and is case insensitive.
+    #   This means that if we pass "er" to the occupation parameter,
+    #   we should include all users with an occupation that contains "er"
+    #   in the results.
+    # Do not include a user in the results more than once.
+
     result = []
    
     id_matches = []
